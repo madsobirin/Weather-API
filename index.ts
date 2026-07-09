f04dcd7483190f6028b1394ab23ab80139ac3f43
madsobirin <gamingbrn202@gmail.com>
2026-07-09 10:15:52 +0700

    update: add mock data dummy weather

--- index.ts
+++ index.ts
@@ -1,1 +1,29 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send("Weather API is running");
+});
+
+app.get("/api/weather/:city", (req, res) => {
+  const { city } = req.params;
+
+  const mocWeather = {
+    city: city,
+    temperature: 30,
+    condition: "sunny",
+    humidity: 70,
+    windSpeed: 10,
+  };
+
+  res.status(200).json({
+    success: true,
+    message: "Weather berhasil ditemukan",
+    data: mocWeather,
+  });
+});
+
+export default app;
